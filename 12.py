5df7ae7cc44697c1bdf225d55be9b2ca19e992a5
Varadi Fruzsina <fruzsina.varadi@thesysadmin.eu>
2018-05-02 19:05:19 +0200

    mirrors

--- 12.py
+++ 12.py
@@ -1,2 +1,17 @@
-import numpy as np
-
+num=[]
+i=100
+j=100
+while i<1000:
+    while j<1000:
+        number=i*j
+        number_string=str(number)
+        ch=number_string[0]
+        ch2=number_string[:-1]
+        if ch==ch2:
+            flag=True
+        else:
+            flag=False
+        if flag==True:
+            ch+=1
+            ch2-=1
+    i+=1
