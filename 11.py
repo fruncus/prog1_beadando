3cede68091820b395578d349a545110df1d89537
Varadi Fruzsina <fruzsina.varadi@thesysadmin.eu>
2018-05-05 13:53:51 +0300

    fg

--- 11.py
+++ 11.py
@@ -1,25 +1,27 @@
 from datetime import datetime
-import math
+from math import sqrt
+
+def nthprim(n):
+    prim=13
+    i=6
+    while i<n:
+        prim+=1
+        flag=True
+        osztok=2
+        if prim%2!=0:
+            j=3
+            while j<(sqrt(prim)+1):
+                if prim%j!=0:
+                    j+=1
+                else:
+                    flag=False
+                    break
+        else:
+            flag=False
+        if osztok==2 and flag==True:
+            i+=1
+    return prim
 
 start = datetime.now()
-prim=13
-i=6
-while i<10001:
-    prim+=1
-    flag=True
-    osztok=2
-    if prim%2!=0:
-        j=3
-        while j<(math.sqrt(prim)+1):
-            if prim%j!=0:
-                j+=1
-            else:
-                flag=False
-                break
-    else:
-        flag=False
-    if osztok==2 and flag==True:
-        i+=1
-
-print(prim)
+print(nthprim(10001))
 print (datetime.now()-start)
