32f349e56cb1708168c4f6de1b43285246d83d6d
Varadi Fruzsina <fruzsina.varadi@thesysadmin.eu>
2018-05-05 14:46:13 +0300

    vegleges

--- 11.py
+++ 11.py
@@ -1,13 +1,36 @@
 from datetime import datetime
 from math import sqrt
 
+
 def nthprim(n):
+    start = datetime.now()
+    l=[2,3,5,7,11,13]
     prim=13
     i=6
     while i<n:
         prim+=1
         flag=True
-        osztok=2
+        for j in l:
+            if j<(sqrt(prim)+1):
+                if prim%j!=0:
+                    continue
+                else:
+                    flag=False
+                    break
+        if flag==True:
+            l.append(prim)
+            i+=1
+    print(datetime.now() - start)
+    return prim
+
+
+def nthprims(n):
+    start = datetime.now()
+    prim=13
+    i=6
+    while i<n:
+        prim+=1
+        flag=True
         if prim%2!=0:
             j=3
             while j<(sqrt(prim)+1):
@@ -18,10 +41,11 @@ def nthprim(n):
                     break
         else:
             flag=False
-        if osztok==2 and flag==True:
+        if flag==True:
             i+=1
+    print(datetime.now() - start)
     return prim
 
-start = datetime.now()
-print(nthprim(10001))
-print (datetime.now()-start)
+
+# print(nthprim(10001))
+print(nthprims(10001))
