932ddb02984b96fb9f066c9229b3c8456c53a712
Varadi Fruzsina <fruzsina.varadi@thesysadmin.eu>
2018-05-08 20:30:52 +0200

    piglatin

--- 4.py
+++ 4.py
@@ -1,15 +1,31 @@
 def translator(text):
     listed=text.split(" ")
+    english=[]
     pig=[]
-    for i in listed:
-        string=i[1:]+i[0].lower()+"ay"
-        pig.append(string)
-    pig[0]=pig[0].capitalize()
-    for i in pig:
-        print(i, end=" ")
+    nyelv=' '
+    for veg in listed:
+        if veg[-2]=='a' and veg[-1]=='y':
+            nyelv='piglatin'
+        else:
+            nyelv='angol'
+
+    if nyelv=='piglatin':
+        for i in listed:
+            helyes=i[-3]+i[:-3]
+            english.append(helyes)
+        english[0]=english[0].capitalize()
+        for i in english:
+            print(i, end=" ")
+    else:
+        for i in listed:
+            string=i[1:]+i[0].lower()+"ay"
+            pig.append(string)
+        pig[0]=pig[0].capitalize()
+        for i in pig:
+            print(i, end=" ")
 
 
 
-phrase=input("írj egy mondatot:")
-print("Pig latinul:")
+phrase=input("Irj egy mondatot: ")
+print("A masik nyelven: ")
 translator(phrase)
