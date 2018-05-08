5f584195b2119745713cd328ab65ccbfd0dedf55
Varadi Fruzsina <fruzsina.varadi@thesysadmin.eu>
2018-05-08 19:25:55 +0200

    palindrome

--- 12.py
+++ 12.py
@@ -1,21 +1,28 @@
-i=999
-j=999
-while i>99:
-    while j>99:
-        number=i*j
-        number_string=str(number)
-        k=int(len(number_string))/2
-        for e in range(int(k)):
-            for f in range (int(len(number_string))-1,int(k),-1):
-                if number_string[e]==number_string[f]:
-                    flag=True
-                    continue
-                else:
-                    flag=False
-                    break
-        if flag==True:
-            print(number)
-            exit()
-    i-=1
-    j-=1
+def palindrome(n):
+    for i in range(994,n,-1):
+        for j in range(999,n,-1):
+            number=i*j
+            number_string=str(number)
+    #     k=int(len(number_string))/2
+    #     for e in range(int(k)):
+    #         for f in range (int(len(number_string))-1,int(k),-1):
+    #             if number_string[e]==number_string[f]:
+    #                 flag=True
+    #                 continue
+    #             else:
+    #                 flag=False
+    #                 break
+    #     if flag==True:
+    #         print(number)
+    #         exit()
+    # i-=1
+    # j-=1
+            k=len(number_string)/2
+            kozepe=int(k)
+            eleje=number_string[0:kozepe]
+            vege=number_string[kozepe:]
+            vege_visszafele=vege[::-1]
+            if eleje==vege_visszafele:
+                return number,i,j
 
+print("the largest palindrome made from the product of two 3-digit numbers is",palindrome(99))
