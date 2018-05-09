bd1f7a7fe3a3e160e5958650f14c567a91f74218
Varadi Fruzsina <fruzsina.varadi@thesysadmin.eu>
2018-05-09 21:32:37 +0200

    finish palindrome

--- 12.py
+++ 12.py
@@ -1,5 +1,6 @@
 def palindrome(n):
-    for i in range(994,n,-1):
+    lista=[]
+    for i in range(999,n,-1):
         for j in range(999,n,-1):
             number=i*j
             number_string=str(number)
@@ -23,6 +24,11 @@ def palindrome(n):
             vege=number_string[kozepe:]
             vege_visszafele=vege[::-1]
             if eleje==vege_visszafele:
-                return number,i,j
+                lista.append(number)
+    max=lista[0]
+    for i in lista:
+        if i>max:
+            max=i
+    return max
 
-print("the largest palindrome made from the product of two 3-digit numbers is",palindrome(99))
+print("the largest palindrome made from the product of two 3 digit numbers is",palindrome(99))
